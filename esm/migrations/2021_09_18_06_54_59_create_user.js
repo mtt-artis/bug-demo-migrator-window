676ff9e00ccc0cf4358085ef2151baef836a950a
mtt <>
2023-07-31 10:52:43 +0200

    fix

--- esm/migrations/2021_09_18_06_54_59_create_user.js
+++ esm/migrations/2021_09_18_06_54_59_create_user.js
@@ -1,12 +1,12 @@
-
+import { sql } from "kysely";
 export async function up(db) {
     await db.schema
         .createTable('user')
-        .addColumn('user_id', 'uuid', (col) => col.primaryKey().defaultTo((0, kysely_1.sql)`gen_random_uuid()`))
+        .addColumn('user_id', 'uuid', (col) => col.primaryKey().defaultTo((0, sql)`gen_random_uuid()`))
         .addColumn('first_name', 'text')
         .addColumn('last_name', 'text')
         .addColumn('email', 'text', (col) => col.unique())
-        .addColumn('created_at', 'timestamp', (col) => col.defaultTo((0, kysely_1.sql)`NOW()`))
+        .addColumn('created_at', 'timestamp', (col) => col.defaultTo((0, sql)`NOW()`))
         .execute();
 }
 export async function down(db) {
